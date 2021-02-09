c588a72b89310ac76a61edc047798bd3e1499282
Adrian velasquez Guerrero <adrian.velasquez.guerrero@gmail.com>
2021-02-08 21:14:26 -0500

    extension live

---
+++ main.js
@@ -0,0 +1,22 @@
+let cadena = "Hola a todos bienvenidos al curso de Javascript";
+
+
+// Verificadar en la cadena concidencias
+
+// let resultado = cadena.startsWith("hola");         //Busca al inicio de la cadena si exite la palabrea y devuelve un boleano como respuesta (true/flase)
+// let resultado = cadena.endsWith("Javascript");     //Busca al final de la cadena si exite la palabrea y devuelve un boleano como respuesta (true/flase)
+// let resultado = cadena.includes("bienvenidos");    //Busca dentro de toda la cadena si exite la palabra y devuelve un boleano como respuesta (true/flase)
+// let resultado = cadena.indexOf("e");               //Busca dentro de toda la cadena la concidencia en la palabra de izquier a deracha y nos devuelvela primera concidencia con su pocicion
+// let resultado = cadena.lastIndexOf("e");           //Busca dentro de toda la cadena la concidencia en la palabra de deracha a izquierda y nos devuelvela primera concidencia con su pocicion
+
+// Modificamos la cadena 
+// let resultado = cadena.repeat(5);                  //Nos repite la cadena de la cantidad de veces que ingresemos
+
+
+//Modificamos pero nos devuelven diferentes tipos de datos (array,numbre,onject)
+
+
+
+
+
+console.log(resultado);
